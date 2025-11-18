fbd1d0f47be63cb00f168e7c6bdb2bbc76627020
anchal-github123 <anchalnayak2003@gmail.com>
2025-11-18 11:44:50 +0530

    vercel setup

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,23 +1,23 @@
-import express from 'express'
+import express from 'express';
 import cors from "cors";
 import { main } from '../genai.js';
-const app=express()
 
+const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.get("/",(req,res)=>{
-    res.send("HII")
-})
 
-app.post("/ask",async (req,res)=>{
+// Test route
+app.get("/", (req, res) => {
+    res.send("HII");
+});
 
-    const userMessage = req.body.message; 
-    let output= await main(userMessage)
-    res.json({sender:"AI",text:output})
-    
-  
-})
+// AI route
+app.post("/ask", async (req, res) => {
+    const userMessage = req.body.message;
+    let output = await main(userMessage);
+    res.json({ sender: "AI", text: output });
+});
 
-
+// Required for Vercel
 export default app;
